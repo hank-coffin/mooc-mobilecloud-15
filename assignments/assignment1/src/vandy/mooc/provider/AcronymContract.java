77268ca1a040ca2c2aa3199ad0e76c54c5a96d7e
hank-coffin <hank.coffin@gmail.com>
2015-07-05 17:21:03 -0700

    new stuff

--- assignments/assignment1/src/vandy/mooc/provider/AcronymContract.java
+++ assignments/assignment1/src/vandy/mooc/provider/AcronymContract.java
@@ -7,7 +7,9 @@ import android.provider.BaseColumns;
 /**
  * Defines table and column names for the Acronym database.
  */
-public final class AcronymContract {
+public final
+class AcronymContract
+{
     /**
      * The "Content authority" is a name for the entire content
      * provider, similar to the relationship between a domain name and
@@ -15,16 +17,13 @@ public final class AcronymContract {
      * authority is the package name for the app, which must be unique
      * on the device.
      */
-    public static final String CONTENT_AUTHORITY =
-        "vandy.mooc.acronym";
+    public static final String CONTENT_AUTHORITY = "vandy.mooc.acronym";
 
     /**
      * Use CONTENT_AUTHORITY to create the base of all URI's that apps
      * will use to contact the content provider.
      */
-    public static final Uri BASE_CONTENT_URI =
-        Uri.parse("content://"
-                  + CONTENT_AUTHORITY);
+    public static final Uri BASE_CONTENT_URI = Uri.parse( "content://" + CONTENT_AUTHORITY );
 
     /**
      * Possible paths (appended to base content URI for possible
@@ -33,65 +32,62 @@ public final class AcronymContract {
      * fail since the ContentProvider hasn't been given any
      * information on what to do with "givemeroot".
      */
-    public static final String PATH_ACRONYM =
-        AcronymEntry.TABLE_NAME;
+    public static final String PATH_ACRONYM = AcronymEntry.TABLE_NAME;
 
     /**
      * Inner class that defines the contents of the Acronym table.
      */
-    public static final class AcronymEntry implements BaseColumns {
+    public static final
+    class AcronymEntry implements BaseColumns
+    {
         /**
          * Use BASE_CONTENT_URI to create the unique URI for Acronym
          * Table that apps will use to contact the content provider.
          */
-        public static final Uri CONTENT_URI = 
-            BASE_CONTENT_URI.buildUpon()
-            .appendPath(PATH_ACRONYM).build();
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath( PATH_ACRONYM ).build();
 
         /**
          * When the Cursor returned for a given URI by the
          * ContentProvider contains 0..x items.
          */
-        public static final String CONTENT_ITEMS_TYPE =
-            "vnd.android.cursor.dir/"
-            + CONTENT_AUTHORITY 
-            + "/" 
-            + PATH_ACRONYM;
+        public static final String CONTENT_ITEMS_TYPE = "vnd.android.cursor.dir/"
+                                                        + CONTENT_AUTHORITY
+                                                        + "/"
+                                                        + PATH_ACRONYM;
 
         /**
          * When the Cursor returned for a given URI by the
          * ContentProvider contains 1 item.
          */
-        public static final String CONTENT_ITEM_TYPE =
-            "vnd.android.cursor.item/"
-            + CONTENT_AUTHORITY 
-            + "/" 
-            + PATH_ACRONYM;
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/"
+                                                        + CONTENT_AUTHORITY
+                                                        + "/"
+                                                        + PATH_ACRONYM;
 
         /**
          * Name of the database table.
          */
-        public static final String TABLE_NAME =
-            "acronym_table";
+        public static final String TABLE_NAME = "acronym_table";
 
         /**
          * Columns to store Data of each Acronym Expansion.
          */
-        public static final String COLUMN_ACRONYM = "acronym";
-        public static final String COLUMN_LONG_FORM = "long_form";
-        public static final String COLUMN_FREQUENCY = "frequency";
-        public static final String COLUMN_SINCE = "since";
+        public static final String COLUMN_ACRONYM         = "acronym";
+        public static final String COLUMN_LONG_FORM       = "long_form";
+        public static final String COLUMN_FREQUENCY       = "frequency";
+        public static final String COLUMN_SINCE           = "since";
         public static final String COLUMN_EXPIRATION_TIME = "expiration_time";
 
         /**
          * Return a Uri that points to the row containing a given id.
-         * 
+         *
          * @param id
          * @return Uri
          */
-        public static Uri buildAcronymUri(Long id) {
-            return ContentUris.withAppendedId(CONTENT_URI,
-                                              id);
+        public static
+        Uri buildAcronymUri( Long id )
+        {
+            return ContentUris.withAppendedId( CONTENT_URI, id );
         }
     }
 }
--- assignments/assignment1/src/vandy/mooc/retrofit/AcronymData.java
+++ assignments/assignment1/src/vandy/mooc/retrofit/AcronymData.java
@@ -8,64 +8,78 @@ import java.util.List;
  * don't care about all the data, just the fields defined in this
  * class.
  */
-public class AcronymData {
+public
+class AcronymData
+{
     /**
      * Various fields corresponding to data downloaded in Json from
      * the Acronym WebService.
      */
-    private String sf;
-    private List<AcronymExpansion> lfs;
+    private String                   sf;
+    private List< AcronymExpansion > lfs;
 
     /**
      * No-op constructor
      */
-    public AcronymData() {
+    public
+    AcronymData()
+    {
     }
 
     /**
      * Constructor that initializes all the fields of interest.
      */
-    public AcronymData(String sf,
-                       List<AcronymExpansion> lfs) {
+    public
+    AcronymData( String sf, List< AcronymExpansion > lfs )
+    {
         super();
-        this.sf = sf;
-        this.lfs = lfs;
+
+        this.sf     = sf;
+        this.lfs    = lfs;
     }
 
     /**
      * @return the acronym
      */
-    public String getSf() {
+    public
+    String getSf()
+    {
         return sf;
     }
 
     /**
      * @param acronym
      */
-    public void setSf(String sf) {
+    public
+    void setSf( String sf )
+    {
         this.sf = sf;
     }
 
     /**
      * @return the List of AcronymData associated with that acronym
      */
-    public List<AcronymExpansion> getLfs() {
+    public
+    List< AcronymExpansion > getLfs()
+    {
         return lfs;
     }
 
     /**
-     * @param the
-     *            List of AcronymData associated with that acronym
+     * @param the List of AcronymData associated with that acronym
      */
-    public void setLfs(List<AcronymExpansion> lfs) {
+    public
+    void setLfs( List< AcronymExpansion > lfs )
+    {
         this.lfs = lfs;
     }
-    
+
     /**
-     *Inner class that contains data for each Acronym Expansion.
-     *
+     * Inner class that contains data for each Acronym Expansion.
      */
-    public static class AcronymExpansion {
+    public static
+    class AcronymExpansion
+    {
         /*
          * These data members are the local variables that will store
          * the AcronymExpansion's state.
@@ -92,39 +106,53 @@ public class AcronymData {
          * Constructor that initialises an AcronymExpansion object
          * from its parameters.
          */
-        public AcronymExpansion(String lf,
-                                int freq,
-                                int since) {
-            this.lf = lf;
-            this.freq = freq;
-            this.since = since;
+        public
+        AcronymExpansion( String lf,
+                          int freq,
+                          int since )
+        {
+            this.lf     = lf;
+            this.freq   = freq;
+            this.since  = since;
         }
 
         /*
          * Getters and setters to access AcronymExpansion.
          */
 
-        public String getLf() {
+        public
+        String getLf()
+        {
             return lf;
         }
 
-        public void setLf(String lf) {
+        public
+        void setLf( String lf )
+        {
             this.lf = lf;
         }
 
-        public int getFreq() {
+        public
+        int getFreq()
+        {
             return freq;
         }
 
-        public void setFreq(int freq) {
+        public
+        void setFreq( int freq )
+        {
             this.freq = freq;
         }
 
-        public int getSince() {
+        public
+        int getSince()
+        {
             return since;
         }
 
-        public void setSince(int since) {
+        public
+        void setSince( int since )
+        {
             this.since = since;
         }
     }
